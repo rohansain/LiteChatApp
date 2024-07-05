a2b9a966efc284e37a7f7b1da4ec043f1616d708
rohan <rohansain708@gmail.com>
2024-07-05 08:03:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+main().then((res)=>{
+    console.log("connection successful");
+}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+app.get('/',(req,res)=>{
+    res.send("root working properly");
+})
+
+app.listen(8080,()=>{
+    console.log("app is listening on port 8080");
+})
