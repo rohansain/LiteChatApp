a443c90d1991d9e53c5968691a9b3f863a07dce4
rohan <rohansain708@gmail.com>
2024-07-08 21:55:34 +0530

    add feature create new chat

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Chat = require('./model/chat.js');
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.urlencoded({extended:true}));
 main().then((res)=>{
     console.log("connection successful");
 }).catch(err => console.log(err));
@@ -23,6 +23,20 @@ app.get('/chats',async (req,res)=>{
     let chats = await Chat.find();
     // console.log(chats);
     res.render("index.ejs",{chats});
+})
+
+app.post('/chats',(req,res)=>{
+    let {sender,msg,receiver} = req.body;
+    let chat = new Chat({from:sender,to:receiver,msg:msg,created_at:new Date()});
+    chat.save().then((data)=>{
+        // console.log(data);
+    }).catch((err)=>{
+        console.log(err._message);
+    })
+    res.redirect("/chats");
+})
+app.get('/chats/new',(req,res)=>{
+    res.render("new.ejs");
 })
 
 app.listen(8080,()=>{
